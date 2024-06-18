79b406a3a55ede0041147d1ec9047c178100b77f
Anna <iaanya27@gmail.com>
2024-06-18 14:38:07 +0300

    added -1000 for dif

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,27 +1,27 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const refs = {
-    datePicker: document.querySelector('#datetime-picker'),
-    btnStart: document.querySelector('[data-start]'),
-    daysEl: document.querySelector('[data-days]'),
-    hoursEl: document.querySelector('[data-hours]'),
-    minutesEl: document.querySelector('[data-minutes]'),
-    secondsEl: document.querySelector('[data-seconds]'),
+  datePicker: document.querySelector('#datetime-picker'),
+  btnStart: document.querySelector('[data-start]'),
+  daysEl: document.querySelector('[data-days]'),
+  hoursEl: document.querySelector('[data-hours]'),
+  minutesEl: document.querySelector('[data-minutes]'),
+  secondsEl: document.querySelector('[data-seconds]'),
 };
-    
+
 let intervalId;
 let initTime;
 
-refs.btnStart.disabled = true; 
+refs.btnStart.disabled = true;
 refs.btnStart.addEventListener('click', () => {
-       intervalId = setInterval(() => {
-         renderTime(initTime); 
-         refs.btnStart.disabled = true; 
-       }, 1000);
-     });
+  intervalId = setInterval(() => {
+    renderTime(initTime);
+    refs.btnStart.disabled = true;
+  }, 1000);
+});
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -44,21 +44,21 @@ function convertMs(ms) {
 
 function renderTime(time) {
   const currentTime = Date.now();
-  const diff = initTime - currentTime; 
+  const diff = initTime - currentTime;
   const obj = convertMs(diff);
-  refs.daysEl.innerText = String(obj.days).padStart(2,  '0');
-  refs.hoursEl.innerText = String(obj.hours).padStart(2,  '0');
-  refs.minutesEl.innerText = String(obj.minutes).padStart(2,  '0');
+  refs.daysEl.innerText = String(obj.days).padStart(2, '0');
+  refs.hoursEl.innerText = String(obj.hours).padStart(2, '0');
+  refs.minutesEl.innerText = String(obj.minutes).padStart(2, '0');
   refs.secondsEl.innerText = String(obj.seconds).padStart(2, '0');
   refs.btnStart.disabled = false;
-  if (diff <= 0) {
-    clearInterval(intervalId); 
-             };
-};
+  if (diff - 1000 <= 0) {
+    clearInterval(intervalId);
+  }
+}
 
 function validateTime(time) {
   return time <= Date.now();
-};
+}
 
 const options = {
   enableTime: true,
@@ -67,16 +67,14 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     initTime = selectedDates[0];
-           if (validateTime(initTime)) {
+    if (validateTime(initTime)) {
       iziToast.error({
         position: `topRight`,
-          title: `error`,
+        title: `error`,
         message: 'Please choose a date in the future',
       });
-    } 
+    }
     refs.btnStart.disabled = false;
   },
 };
 flatpickr('#datetime-picker', options);
-
-    
